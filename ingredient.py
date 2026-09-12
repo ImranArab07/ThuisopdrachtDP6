d4c3b725e8cbcf4e84bc6a477814c50ba38257ad
ImranArab07 <Imran.a07@hotmail.com>
2026-09-12 18:06:45 +0200

    feat(FR-3): plantaardig alternatieven toevoegen

--- ingredient.py
+++ ingredient.py
@@ -3,7 +3,22 @@ class Ingredient:
         self.__naam = naam
         self.__hoeveelheid = hoeveelheid
         self.__eenheid = eenheid
+        self.__plantaardig_alternatief = None  # Optioneel plantaardig alternatief
 
     def __str__(self):
         return f"{self.__hoeveelheid} {self.__eenheid} {self.__naam}"
-    
+
+    def set_plantaardig_alternatief(
+            self, 
+            alternatief: "Ingredient"
+            ) -> None:
+        self.__plantaardig_alternatief = alternatief
+
+    def get_ingredient(self, plantaardig: bool) -> "Ingredient":
+        if plantaardig and self.__plantaardig_alternatief is not None:
+            return self.__plantaardig_alternatief
+        
+        return self
+
+    def __str__(self):
+        return f"{self.__hoeveelheid} {self.__eenheid} {self.__naam}"
