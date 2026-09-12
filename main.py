98051cd898a3626210ef8d47b6dbb7c79f8c4c72
ImranArab07 <Imran.a07@hotmail.com>
2026-09-12 15:49:35 +0200

    feat(FR-1): ingredienten van recept 2 en stappen toegevoegd

--- main.py
+++ main.py
@@ -28,7 +28,29 @@ def main():
     kip_kerrie.voeg_stap_toe(Stap("Serveer de kip-kerrie met de rijst, sperziebonen en broccoli."))
     recepten.append(kip_kerrie)
 
-    gehakt_quiche = Recept("Gehakt quiche met paprika", "Een heerlijke quiche met gehakt en paprika.")
+    gehakt_quiche = Recept("Gehakt quiche met paprika", "Een heerlijke quiche met gehakt, kaas en paprika.")
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("rundergehakt", 400, "gram"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient(" rode paprika", 1, "stuk"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("ui", 1, "stuk"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("quichedeeg", 2, "vellen"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("ei", 1, "stuk"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("knoflook", 2, "teentjes"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("geraspte kaas", 150, "gram"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("eieren", 3, "stuk"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("kookroom", 200, "milliliter"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("zout", 1, "theelepel"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("peper", 1, "theelepel"))
+    gehakt_quiche.voeg_ingredient_toe(Ingredient("tijm", 1, "theelepel"))
+
+    gehakt_quiche.voeg_stap_toe(Stap("Verwarm de oven voor op 180 graden Celsius."))
+    gehakt_quiche.voeg_stap_toe(Stap("Snijd de ui, knoflook en paprika in kleine stukjes."))
+    gehakt_quiche.voeg_stap_toe(Stap("Bak het gehakt in een pan totdat het bruin is. Voeg de ui, knoflook en paprika toe en bak dit mee totdat de groenten zacht zijn."))
+    gehakt_quiche.voeg_stap_toe(Stap("Bekleed een quichevorm met het quichedeeg en prik gaatjes in de bodem met een vork."))
+    gehakt_quiche.voeg_stap_toe(Stap("Klop de eieren los met de kookroom, zout, peper en tijm."))
+    gehakt_quiche.voeg_stap_toe(Stap("Voeg het gehaktmengsel toe aan het eimengsel en roer goed door."))
+    gehakt_quiche.voeg_stap_toe(Stap("Giet het mengsel in een ingevette quichevorm en bestrooi met geraspte kaas."))
+    gehakt_quiche.voeg_stap_toe(Stap("Bak de quiche in de voorverwarmde oven gedurende 30-35 minuten tot hij goudbruin is."))
+
     recepten.append(gehakt_quiche)
 
 if __name__ == "__main__":
