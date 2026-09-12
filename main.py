67c98e048f408df18732b9cdb2cd5cd4dd0822dd
ImranArab07 <Imran.a07@hotmail.com>
2026-09-12 15:42:38 +0200

    feat(FR-1): stappen van recept 1 toegevoegs

--- main.py
+++ main.py
@@ -20,7 +20,16 @@ def main():
     kip_kerrie.voeg_ingredient_toe(Ingredient("knoflook", 2, "teentjes"))
     kip_kerrie.voeg_ingredient_toe(Ingredient("kerriepoeder", 1, "eetlepel"))
 
-    
+    kip_kerrie.voeg_stap_toe(Stap("Kook de rijst en zet een pan water met een snuf zout op het vuur voor de sperziebonen."))
+    kip_kerrie.voeg_stap_toe(Stap("Snijd de kip in kleine blokjes, snipper het uitje, snijd de knoflook fijn en snijd de kontjes van de sperziebonen en snijd de broccoli (was de sperziebonen en broccoli ook even)."))
+    kip_kerrie.voeg_stap_toe(Stap("Bak de kip in een beetje olie goudbruin. Voeg de ui en knoflook toe en bak dit even mee. Voeg de kerriepoeder toe en bak dit kort mee."))
+    kip_kerrie.voeg_stap_toe(Stap("Voeg de kokosmelk toe en laat het geheel 10 minuten zachtjes koken."))
+    kip_kerrie.voeg_stap_toe(Stap("Kook ondertussen de sperziebonen in 5 minuten beetgaar en kook de broccoli in 4 minuten beetgaar."))
+    kip_kerrie.voeg_stap_toe(Stap("Serveer de kip-kerrie met de rijst, sperziebonen en broccoli."))
+    recepten.append(kip_kerrie)
+
+    gehakt_quiche = Recept("Gehakt quiche met paprika", "Een heerlijke quiche met gehakt en paprika.")
+    recepten.append(gehakt_quiche)
 
 if __name__ == "__main__":
     main()
