f3013f30c68488a08b632a7e0c04e9cc2a52244c
ImranArab07 <Imran.a07@hotmail.com>
2026-09-12 15:35:45 +0200

    feat(FR-1): ingridient 1 toegevoegd

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+from ingredient import Ingredient
 from recept import Recept 
-#from ingrediënt import Ingredient
-#from stap import Stap
+from stap import Stap
+
+
 
 def main():
     recepten = []
 
-    recept1 = Recept("Kip Kerrie", "Kip kerrie zonder pakjes en zakjes")
-    #recept1.voeg_ingredient_toe(Ingredient("kip", 500, "gram"))
-    #recept1.voeg_ingredient_toe(Ingredient("sperziebonen", 400, "gram"))
+    kip_kerrie = Recept(
+        "Kip Kerrie", 
+        "Een romige kip-kerrie met broccoli, sperziebonen en rijst."
+     )
+    kip_kerrie.voeg_ingredient_toe(Ingredient("kipfilet", 300, "gram"))
+    kip_kerrie.voeg_ingredient_toe(Ingredient("basmatirijst", 200, "gram"))
+    kip_kerrie.voeg_ingredient_toe(Ingredient("sperziebonen", 150, "gram"))
+    kip_kerrie.voeg_ingredient_toe(Ingredient("broccoli", 200, "gram"))
+    kip_kerrie.voeg_ingredient_toe(Ingredient("kokosmelk", 100, "milliliter"))
+    kip_kerrie.voeg_ingredient_toe(Ingredient("ui", 1, "stuk"))
+    kip_kerrie.voeg_ingredient_toe(Ingredient("knoflook", 2, "teentjes"))
+    kip_kerrie.voeg_ingredient_toe(Ingredient("kerriepoeder", 1, "eetlepel"))
 
-    #recept1.voeg_stap_toe(Stap("Kook de rijst en zet een pan water met een snuf zout op het vuur voor de sperziebonen."))
-    #recept1.voeg_stap_toe(Stap("Snijd de kip in kleine blokjes, snipper het uitje, snijd de knoflook fijn en snijd de kontjes van de sperziebonen (was ze ook even)."))
-
-    #recepten.append(recept1)
-
-    recept2 = Recept("Gehakt quiche met paprika", "Een heerlijke quiche met gehakt en paprika.")
-
-    # enz.. enz..
-    # Veel succes!
+    
 
 if __name__ == "__main__":
     main()
